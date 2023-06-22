ae1cff02ba35a735a2fb48a9eb1a277e8a2be012
Ubuntu <ubuntu@ip-172-31-14-255.ap-south-1.compute.internal>
2023-06-22 05:55:12 +0000

    this is integrated with keyvault

---
+++ terraformfiles/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "Final-Project"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
