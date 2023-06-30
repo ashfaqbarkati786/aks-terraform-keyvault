10721fd275fa56c939a45b8207277b5ac1e81669
ashfaqbarkati786 <107784646+ashfaqbarkati786@users.noreply.github.com>
2023-06-30 10:43:17 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "Final-Project"
-  location = "eastus"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks"
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+    vm_size    = var.vm_size
   }
 
   identity {
@@ -20,10 +20,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   key_vault_secrets_provider {
-    secret_rotation_enabled = true
+    secret_rotation_enabled = var.secret_rotation_enabled
   }
 
-  tags = {
-    Environment = "Production"
-  }
+  tags = var.tags
 }
